554cddfac6b02479ea0c148cf3a23cbff9b183b1
Ryan <ryanbiy01@gmail.com>
2023-03-17 12:32:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record){
+    let result = record.find( record => record.result === "W" );
+    const  sadReality =  result ? result.year : undefined;
+    return sadReality;
+    }
+
+superbowlWin()
